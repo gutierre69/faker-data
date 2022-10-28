04e479ca0bd98fc2bc70a5ed5b232425319f5a2a
gutierre69 <gutierre69@gmail.com>
2022-10-28 07:57:33 -0300

    update example

--- examples/index.php
+++ examples/index.php
@@ -3,19 +3,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require __DIR__.'/vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use FakerData\Generate;
 
 
 $FakerData = new Generate();
 
-print_r( $FakerData->phone );
-
-// echo $FakerData->person->first_name."<br>";
-// echo $FakerData->person->last_name."<br>";
-// echo $FakerData->person->complete_name."<br>";
-// echo $FakerData->person->picture."<br>";
+echo $FakerData->person->first_name."<br>";
+echo $FakerData->person->last_name."<br>";
+echo $FakerData->person->complete_name."<br>";
+echo $FakerData->person->picture."<br>";
 
 // echo nl2br( print_r($FakerData->person, true) );
 
